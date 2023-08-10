5c4bbab9d7c1d6ab1543e2ab6eb3c747d878ca4c
joseaha <jose.jaha.jh@gmail.com>
2023-08-09 22:10:38 -0600

    Creacion del modelo UserModel

---
+++ CRUD_ASP7/Models/UserModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUD_ASP7.Models
+{
+    public class UserModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "The user is required")]
+        [StringLength(20)]
+        public string User { get; set; }
+
+        [Required(ErrorMessage = "The user is Email")]
+        [DataType(DataType.EmailAddress)]
+        [StringLength(255)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "The user is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
